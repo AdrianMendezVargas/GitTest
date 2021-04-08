485e6ec8a6921b6bf19f2e786cf9e8288acd99b4
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2021-04-08 12:59:18 -0400

    Revert "Agregando mala palabra"
    This reverts commit 101619029106d8d210fa18c5362fdc478bf827b1.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Git_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola marika mundo!");
+            Console.WriteLine("Hola mundo!");
         }
     }
 }
